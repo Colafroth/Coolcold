1f7ba7790fb1633ced030e07a5604e677135e45c
AnTeng Lin <at.tonylin@gmail.com>
2019-04-10 09:27:54 +1000

    debug

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ module.exports.loop = function () {
     // console.log('Harvesters: ' + harvesters.length);
     
 
-    console.log('Value: ${Game.creeps.length}')
+    console.log(`Value: ${Game.creeps.length}`)
 
     if(Game.creeps.length % 4 == 2) {
         var newName = 'Upgrader' + Game.time;
