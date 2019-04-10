ff642b2875525d30311e4d0f83e7d3b8ed768da4
AnTeng Lin <at.tonylin@gmail.com>
2019-04-10 13:18:38 +1000

    modify harvestor logic

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -16,6 +16,13 @@ var roleHarvester = {
                         structure.energy < structure.energyCapacity;
                 }
             });
+            if (targets.length === 0) {
+                targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return structure.structureType == STRUCTURE_SPAWN
+                    }
+                });
+            }
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
